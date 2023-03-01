07f1801cc2d69ff3048b22e04f93069dbce188e9
shraiber <shraiber@ujin.tech>
2023-03-01 15:34:26 +0200

    Initial Commit

---
+++ SparrowMararthon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowMararthon
+//
+//  Created by Dmitriy Shrayber on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
